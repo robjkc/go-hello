d686bca68cc3e5326f71bb5d0aaef9cd6dd658b4
Robert Johnson <robjkc@yahoo.com>
2023-01-19 16:27:27 -0600

    add go endpoint

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/go", HelloGo)
 	fmt.Println("Listening on 8082")
 	http.ListenAndServe(":8082", nil)
 }
@@ -14,4 +15,13 @@ func main() {
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	fmt.Println("Hello endpoint hit!")
+}
+
+func HelloGo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Max-Age", "15")
+
+	fmt.Fprintf(w, "{\"msg\":\"Hello from Go New version!\"}")
+	fmt.Println("Go endpoint hit!")
 }
