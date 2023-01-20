239dfa2b2af0da2c87d8d74d16d080bb0ff557bf
Robert Johnson <robjkc@yahoo.com>
2023-01-20 08:08:06 -0600

    little vpn work

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/go", HelloGo)
+	http.HandleFunc("/control-system", GetControlSystem)
+
 	fmt.Println("Listening on 8082")
 	http.ListenAndServe(":8082", nil)
 }
@@ -22,6 +25,21 @@ func HelloGo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Max-Age", "15")
 
-	fmt.Fprintf(w, "{\"msg\":\"Hello from Go New version!\"}")
+	fmt.Fprintf(w, "{\"msg\":\"Hello from Go New version1!\"}")
 	fmt.Println("Go endpoint hit!")
+}
+
+func GetControlSystem(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Max-Age", "15")
+
+	fmt.Println("Control System endpoint hit!")
+
+	url := "https://apid.securecomwireless.com/v2/control_systems/12345?auth_token=wR8aZxxZAuhyhtaH2iyb"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Fprintf(w, string(body))
 }
